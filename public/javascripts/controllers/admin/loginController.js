b35506a1cac8e845556f8d3f3ad2c302910221e8
monteskier <jmontoliu7@gmail.com>
2017-06-22 14:07:56 +0200

    autocorreció 2

--- public/javascripts/controllers/admin/loginController.js
+++ public/javascripts/controllers/admin/loginController.js
@@ -1,24 +1,39 @@
 angular.module("gentGran")
 .controller("LoginController", ['$scope', '$location', '$http', '$rootScope', '$timeout', function($scope, $location, $http, $rootScope, $timeout ){
+  $scope.logout = function(){
 
+  if($rootScope.login=="logout"){
+    $http({
+      method:"get",
+      url:"admin/logout"
+    }).then(function(results){
+      $rootScope.msg = results.data.msg;
+      $rootScope.flag=false;
+      $rootScope.login="login";
+      $rootScope.session=results.data.session;
+      window.location.replace('/');
+      $timeout(setTimeout,3000);
+    });
+  }
+};
   $scope.login = function(){
     if($rootScope.login == 'login'){
       $http({
         method:"POST",
         url:"admin/login",
         data:{name:$scope.name, password:$scope.password}
-      }).then(function succes(request){
-          if(request.data.login==true){
-            $rootScope.msg = request.data.msg;
-            $rootScope.flag=true;
-            
-            $rootScope.session = request.data.session;
-            $rootScope.login="logout";
-            $timeout(setTimeout,3000);
+      }).then(function(results){
+        if(results.data.login){
+          $rootScope.msg = results.data.msg;
+          $rootScope.flag=true;
+          $rootScope.session=results.data.session;
+          $rootScope.login="logout";
+          $location.path('llista');
+          $timeout(setTimeout,3000);
         }else{
-            $rootScope.flag=true;
-            $rootScope.msg = request.data.msg;
-            $timeout(setTimeout,3000);
+          $rootScope.msg = results.data.msg;
+          $rootScope.flag=true;
+          $timeout(setTimeout,3000);
         }
       });
     }
